2c9d053ae0b9460de8d28af4d2cb2fabaea8ea95
luizguil99 <124329344+luizguil99@users.noreply.github.com>
2025-06-09 17:10:35 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import clientesRouter from './routes/clientes.js';
 
-import app from 'express';
+const app = express();
 
 app.use(express.json());
 app.use('/clientes', clientesRouter);
